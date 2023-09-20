efc594eeda8b81aefcd2a49d1865ef585322addf
Santhosh <shanmugamsanthosh22@gmail.com>
2023-09-20 19:12:32 +0530

    ithinkitlast

---
+++ service_worker.js
@@ -0,0 +1,18 @@
+// Cached core static resources 
+self.addEventListener("install",e=>{
+	e.waitUntil(
+	  caches.open("static").then(cache=>{
+		
+		return cache.addAll(["./","./index.html","./Jarvis.png","./index.js","./service_worker.js"]);
+	  })
+	);
+  });
+  
+  // Fatch resources
+  self.addEventListener("fetch",e=>{
+	e.respondWith(
+	  caches.match(e.request).then(response=>{
+		return response||fetch(e.request);
+	  })
+	);
+  });
